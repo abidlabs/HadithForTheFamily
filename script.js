6ddc8d67351dce0d2f97dd0e2554a11cef7f4bea
Mubashir <mubashir.irfan37@gmail.com>
2021-06-20 15:19:54 +0500

    Implement Loader
    Signed-off-by: Mubashir <mubashir.irfan37@gmail.com>

--- script.js
+++ script.js
@@ -5,10 +5,18 @@ const hadith = document.getElementById("hadith");
 const book = document.getElementById("book");
 const apply = document.getElementById("application");
 
+const contentDiv = document.getElementById('content')
+const loader = document.getElementById('loader')
+
+
 const GET_PUBLIC_SHEET_CELLS = "https://spreadsheets.google.com/feeds/cells/1OzOb4jUF0OY32RyHuEg1wqGioOkeR76Y5cfpEXfal1s/1/public/full?alt=json";
+
+var loading = true;
+
 
 // Sheet Reading Engine
 sheetReaders = {
+  onLoadSuccess: disableLoading,
   publicSheetRawCells : {
     // Reads a Public Sheet's Raw Response -> Parses it to JSON -> Extracts the 'feed.entry' which carries all cells of sheets in an array
     URL:GET_PUBLIC_SHEET_CELLS,
@@ -20,6 +28,7 @@ sheetReaders = {
         hadith.textContent = response[response.length - 3].content.$t;
         book.textContent = response[response.length - 2].content.$t.toUpperCase();
         apply.textContent = response[response.length - 1].content.$t;
+        return true;
       }
     }
   }
@@ -35,8 +44,8 @@ function fetchSheetData() {
 
   xhttp.onload = (response) => {
     response = sheetReader.responseExtractor(response);
-    // Response received. Invoke the reader;
-    sheetReader.responseInterpreters.lastRowReader(response);
+    const loadResult = sheetReader.responseInterpreters.lastRowReader(response);
+    if (loadResult) sheetReaders.onLoadSuccess();
   };
 
   xhttp.open("GET",sheetReader.URL);
@@ -45,9 +54,20 @@ function fetchSheetData() {
   return xhttp;
 }
 
-
 function loadPageData() {
+  enableLoading();
   fetchSheetData();
+}
+
+function enableLoading() {
+  contentDiv.style.display = 'none';
+  loader.style.display = 'block';
+}
+
+function disableLoading() {
+  loading = false;
+  contentDiv.style.display = 'flex';
+  loader.style.display = 'none';
 }
 
 window.onload = loadPageData;
