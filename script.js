6eb35834e343f9aba6d89bf3544ab06fab8e6ff8
Mubashir <mubashir.irfan37@gmail.com>
2021-06-20 14:47:03 +0500

    Design Fixing
    Signed-off-by: Mubashir <mubashir.irfan37@gmail.com>

--- script.js
+++ script.js
@@ -1,37 +1,36 @@
-
-
 // Global Fields
+const date = document.getElementById("date");
+const topic = document.getElementById("topic");
 const hadith = document.getElementById("hadith");
-const reporter = document.getElementById("reporter");
-const source = document.getElementById("source");
-const arabic = document.getElementById("arabic");
-const translation = document.getElementById("translation");
-const apply = document.getElementById("apply");
+const book = document.getElementById("book");
+const apply = document.getElementById("application");
 
+const GET_PUBLIC_SHEET_CELLS = "https://spreadsheets.google.com/feeds/cells/1OzOb4jUF0OY32RyHuEg1wqGioOkeR76Y5cfpEXfal1s/1/public/full?alt=json";
 
 function fetchSheetData() {
   xhttp = new XMLHttpRequest();
 
   xhttp.onload = (response) => {
-    // const hadith = document.getElementById("hadith");
-    // const reporter = document.getElementById("reporter");
-    // const source = document.getElementById("source");
-    // const translation = document.getElementById("translation");
-    // const apply = document.getElementById("apply");
-
     response = JSON.parse(response.currentTarget.response).feed.entry;
     readLastRow(response);
-
-
   };
-  xhttp.open("GET", "https://spreadsheets.google.com/feeds/cells/1OzOb4jUF0OY32RyHuEg1wqGioOkeR76Y5cfpEXfal1s/1/public/full?alt=json");
+
+  xhttp.open("GET", GET_PUBLIC_SHEET_CELLS);
   xhttp.send();
+
+  return xhttp;
 }
 
 function readLastRow(response) {
-  hadith.textContent += response[response.length - 5].content.$t;
-  reporter.textContent += response[response.length - 4].content.$t
-  source.textContent += response[response.length - 3].content.$t;
-  translation.textContent += response[response.length - 2].content.$t;
-  apply.textContent += response[response.length - 1].content.$t
+  date.textContent = response[response.length - 5].content.$t;
+  topic.textContent = response[response.length - 4].content.$t.toUpperCase();
+  hadith.textContent = response[response.length - 3].content.$t;
+  book.textContent = response[response.length - 2].content.$t.toUpperCase();
+  apply.textContent = response[response.length - 1].content.$t;
 }
+
+function loadPageData() {
+  fetchSheetData();
+}
+
+window.onload = loadPageData;
