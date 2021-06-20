20f1ba9bdf02ad1eaa09c605e9167823acc406ab
Mubashir <mubashir.irfan37@gmail.com>
2021-06-20 14:58:37 +0500

    Refactor Sheet Reading Logic into a Centralised Structure
    Signed-off-by: Mubashir <mubashir.irfan37@gmail.com>

--- script.js
+++ script.js
@@ -7,27 +7,44 @@ const apply = document.getElementById("application");
 
 const GET_PUBLIC_SHEET_CELLS = "https://spreadsheets.google.com/feeds/cells/1OzOb4jUF0OY32RyHuEg1wqGioOkeR76Y5cfpEXfal1s/1/public/full?alt=json";
 
+// Sheet Reading Engine
+sheetReaders = {
+  publicSheetRawCells : {
+    // Reads a Public Sheet's Raw Response -> Parses it to JSON -> Extracts the 'feed.entry' which carries all cells of sheets in an array
+    URL:GET_PUBLIC_SHEET_CELLS,
+    responseExtractor : response => JSON.parse(response.currentTarget.response).feed.entry,
+    responseInterpreters: {
+      lastRowReader: function readLastRow(response) {
+        date.textContent = response[response.length - 5].content.$t;
+        topic.textContent = response[response.length - 4].content.$t.toUpperCase();
+        hadith.textContent = response[response.length - 3].content.$t;
+        book.textContent = response[response.length - 2].content.$t.toUpperCase();
+        apply.textContent = response[response.length - 1].content.$t;
+      }
+    }
+  }
+}
+
+/**
+ * Function for fetching Google Sheets Data
+ * This function invokes the response reader function. For changing the response interpretation, change the used sheetReader
+ */
 function fetchSheetData() {
   xhttp = new XMLHttpRequest();
+  const sheetReader = sheetReaders.publicSheetRawCells;
 
   xhttp.onload = (response) => {
-    response = JSON.parse(response.currentTarget.response).feed.entry;
-    readLastRow(response);
+    response = sheetReader.responseExtractor(response);
+    // Response received. Invoke the reader;
+    sheetReader.responseInterpreters.lastRowReader(response);
   };
 
-  xhttp.open("GET", GET_PUBLIC_SHEET_CELLS);
+  xhttp.open("GET",sheetReader.URL);
   xhttp.send();
 
   return xhttp;
 }
 
-function readLastRow(response) {
-  date.textContent = response[response.length - 5].content.$t;
-  topic.textContent = response[response.length - 4].content.$t.toUpperCase();
-  hadith.textContent = response[response.length - 3].content.$t;
-  book.textContent = response[response.length - 2].content.$t.toUpperCase();
-  apply.textContent = response[response.length - 1].content.$t;
-}
 
 function loadPageData() {
   fetchSheetData();
