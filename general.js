4d65eb732867e28a9fc0fa31b4a891901b69f404
Mubashir <mubashir.irfan37@gmail.com>
2021-06-30 22:05:01 +0500

    Code Refactoring
    Signed-off-by: Mubashir <mubashir.irfan37@gmail.com>

--- general.js
+++ general.js
@@ -83,27 +83,6 @@ function setBackgroundImage() {
   else root.style.backgroundImage = "url('bg_low_res.jpg')";
 }
 
-function toggleNav() {
-  navOpened = !navOpened;
-  if (navOpened) openNav();
-  else closeNav();
-}
-
-function openNav() {
-  document.getElementById("sidenav").style.right = "0vw";
-  document.getElementById("navIcon").style.right = "9.5rem";
-  document.getElementById("parent").style.right = "15.625rem";
-
-  navOpened = true;
-}
-
-function closeNav() {
-  document.getElementById("sidenav").style.right = "-15.625rem";
-  document.getElementById("navIcon").style.right = "-6.25rem";
-  document.getElementById("parent").style.right = "0rem";
-
-  navOpened = false;
-}
 
 function showContent(pageName) {
   if (pages.indexOf(pageName) < 0) { window.location.reload()}
