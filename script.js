4746fc69c995a610e4348b632ede25df8735f2b1
Mubashir <mubashir.irfan37@gmail.com>
2021-06-30 10:41:38 +0500

    Signup Success State + Email Format/Registeration Validation
    Signed-off-by: Mubashir <mubashir.irfan37@gmail.com>

--- script.js
+++ script.js
@@ -12,6 +12,9 @@ const homeDiv = document.getElementById('home')
 const loader = document.getElementById('loader')
 const root = document.getElementsByTagName('html')[0];
 
+const userEmailInput = document.getElementById("userEmail");
+const signup = document.getElementById("signUp");
+
 
 const GET_PUBLIC_SHEET_CELLS = "https://spreadsheets.google.com/feeds/cells/1OzOb4jUF0OY32RyHuEg1wqGioOkeR76Y5cfpEXfal1s/1/public/full?alt=json";
 
@@ -113,6 +116,51 @@ function showContent(pageName) {
   currentPage = pageName;
   closeNav();
   return currentPage;
+}
+
+
+function dailyEmailSignup() {
+  var email = document.getElementById("userEmail").value;
+  const submitError = document.getElementById('signupError');
+
+  if(!isValidEmail(email)) {
+    submitError.innerHTML = 'Invalid Email';
+    submitError.style.display = 'block';
+    return;
+  }
+
+  // check if provided email is already subscribed
+  const dbRef = firebase.database().ref();
+  dbRef.child("users").child(email.split('@')[0]).get().then((snapshot) => {
+    if (snapshot.exists()) {
+      snapshot = snapshot.val();
+      if (snapshot.email) {
+        if (snapshot.email === email) {
+          submitError.innerHTML = 'Email already subscribed';
+          submitError.style.display = 'block';
+        }
+      }
+    } else {
+      var usersRef = firebase.database().ref('users/'+email.split('@')[0]);
+
+      usersRef.set({
+        email: email,
+      }, (error) => {
+        if (error) {
+          // The write failed...
+        } else {
+          signup.classList.add('success');
+          signup.innerHTML = "✔ Signed up"
+          userEmailInput.classList.add('submitted');
+          submitError.style.display = 'none';
+          submitError.innerHTML = '';
+        }}
+      );
+    }
+  }).catch((error) => {
+    console.error(error);
+  });
+
 }
 
 window.onload = loadPageData;
---
+++ utils.js
@@ -0,0 +1,9 @@
+function isValidEmail(email) {
+  if (
+    /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(
+     email
+    )
+  )
+    return true;
+  return false;
+}
